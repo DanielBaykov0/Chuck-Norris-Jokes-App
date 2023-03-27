6a284ba7a8fbe2aa43078ad61d8d0242c858d128
Daniel Baykov <danibaikov@abv.bg>
2023-03-27 19:36:01 +0300

    initial commit

---
+++ src/test/java/baykov/daniel/jokesapp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package baykov.daniel.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
